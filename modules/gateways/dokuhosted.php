c08188ee79385d27b7f461a216b9c2eb631e3e9e
Aryo Prakarsa <fojameru@gmail.com>
2017-12-12 17:26:43 +0700

    update for resize button paynow

--- modules/gateways/dokuhosted.php
+++ modules/gateways/dokuhosted.php
@@ -8,7 +8,6 @@
  * This Module Information:
  --------------------------
  PT Nusa Satu Inti Artha (DOKU)
- Version: V1.2
  Released: Sept 18, 2017
  --------------------------
  *
@@ -186,7 +185,6 @@ function dokuhosted_config() {
             'Value' => 'DOKU Merchant',
 			'Description' => 'DOKU is an online payment platform that processes payments through many different methods, including Credit Card, ATM Transfer and DOKU Wallet. Check us out on http://www.doku.com',
         ),
-<<<<<<< HEAD
 		'Pembatas-Description-Payment-Gateway' => array(
 			'FriendlyName' => '',
 			'Type' => 'hidden',
@@ -202,8 +200,6 @@ function dokuhosted_config() {
 			'Default' => 'DOKU is an online payment platform that processes payments through many different methods, including Credit Card, ATM Transfer and DOKU Wallet. Check us out on http://www.doku.com',
 			'Description' => '',
 		),
-=======
->>>>>>> ea563bf7e9f21f25c55eb117477e51594c680776
         // a text field type allows for single line text input
 		# Development
 		//-------------
@@ -721,14 +717,10 @@ function dokuhosted_link($params) {
     $postfields['callback_url'] = $systemUrl . ((substr($systemUrl, -1) == '/') ? '' : '/') . 'modules/gateways/callback/' . $moduleName . '.php';
     $postfields['return_url'] = $returnUrl;
 	// Create Unique invoice_id
-<<<<<<< HEAD
-	//-------------------------------
-	//$postfields['invoice_id'] = $postfields['invoice_id'] . '_' . (date('YmdHis', time()));
+	//===============================
+	#$postfields['invoice_id'] = $postfields['invoice_id'] . '_' . (date('YmdHis', time()));
 	$postfields['invoice_id'] = "{$postfields['invoice_id']}_{$postfields['invoice_id']}";
-	//-------------------------------
-=======
-	$postfields['invoice_id'] = $postfields['invoice_id'] . '_' . (date('YmdHis', time()));
->>>>>>> ea563bf7e9f21f25c55eb117477e51594c680776
+	//===============================
 	####postfields['invoice_id'] = $postfields['invoice_id'];
 	// AVAILABLE PAYMENT CHANNELS
 	$PaymentChannels = array();
@@ -1163,11 +1155,6 @@ function dokuhosted_link($params) {
 	// DOKU Description
 	$dokuDesc = (isset($params['Description-Payment-Gateway']) ? $params['Description-Payment-Gateway'] : '');
 	$dokuDesc = filter_var($dokuDesc, FILTER_SANITIZE_STRING);
-<<<<<<< HEAD
-	$returnHtml .= '<div class="row">';
-	$returnHtml .= '<div class="small-text"><img alt="doku" src="http://doku.com/themes/default/images/logo-doku-merchant.png" align="left" style="padding-right:12px;" />' . $dokuDesc . '</div>';
-	$returnHtml .= '</div>';
-=======
 	$dokuDesc = (is_string($dokuDesc) ? $dokuDesc : '');
 	//===========================
 	// DOKU Description as NULL
@@ -1175,10 +1162,9 @@ function dokuhosted_link($params) {
 	$dokuDesc = "";
 	if (strlen($dokuDesc) > 0) {
 		$returnHtml .= '<div class="row">';
-		$returnHtml .= '<div class="small-text"><img alt="doku" src="http://doku.com/themes/default/images/logo-doku-merchant.png" align="left" style="padding-right:12px;" />' . $dokuDesc . '</div>';
+		$returnHtml .= '<div class="small-text"><img alt="doku" src="http://doku.com/themes/default/images/logo-doku-merchant.png" align="left" width="120" height="34" style="padding-right:12px;" />' . $dokuDesc . '</div>';
 		$returnHtml .= '</div>';
 	}
->>>>>>> ea563bf7e9f21f25c55eb117477e51594c680776
 	
 	//Javascript for payment-channel-placeholder
 	$js = "";
@@ -1315,11 +1301,7 @@ function dokuhosted_link($params) {
 	element.setAttribute("value", "TENOR-VALUE");
 	*/
 	// Submit button
-<<<<<<< HEAD
-	$returnHtml .= '<a href="javascript:;" onclick="javascript:onSubmitToPayment();"><img alt="'.$params['langpaynow'].'" src="/modules/gateways/dokuhosted/assets/doku-button.png" title="'.$params['langpaynow'].'" /></a>';
-=======
 	$returnHtml .= '<a href="javascript:;" onclick="javascript:onSubmitToPayment();"><img alt="'.$params['langpaynow'].'" src="/modules/gateways/dokuhosted/assets/doku-button.png" width="120" height="34" title="'.$params['langpaynow'].'" /></a>';
->>>>>>> ea563bf7e9f21f25c55eb117477e51594c680776
 	
 	// Javascript for submit
 	$returnHtml .= $js;
